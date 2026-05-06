f4a27084bfde10ddb3b9a823d97f2d38f313f2c6
uzupiyo <uzumeron@gmail.com>
2026-05-06 15:57:52 +0900

    Route start to character select

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/stage_select/stage_select.tscn")
+	get_tree().change_scene_to_file("res://scenes/character_select/character_select.tscn")
 
 func _on_collection_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/collection/collection.tscn")
