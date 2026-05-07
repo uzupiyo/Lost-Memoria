7cfb5445a6d7aeccb0f8bd94a9f54715778589a9
uzupiyo <uzumeron@gmail.com>
2026-05-07 23:50:23 +0900

    Add character select color override script

---
+++ scripts/character_select_character_colors.gd
@@ -0,0 +1,14 @@
+extends "res://scripts/character_select.gd"
+
+const UI_COLOR_KAEDE_GREEN: Color = Color(0.55, 1.0, 0.60, 1.0)
+
+func _character_accent_color(character_id: String) -> Color:
+	match character_id:
+		"Rin":
+			return UI_COLOR_RESTORATION_GOLD
+		"Moka":
+			return UI_COLOR_FRAGMENT_PINK
+		"Kaede":
+			return UI_COLOR_KAEDE_GREEN
+		_:
+			return UI_COLOR_DREAM_VIOLET
